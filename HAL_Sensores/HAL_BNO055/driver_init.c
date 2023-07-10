9cebc07a9017dd2b659d6f711fac71f5710c3765
GuilhermeRS11 <guilhermeribeiro201342@gmail.com>
2023-07-10 19:42:03 -0300

    Implementação do HAL_BNO055

---
+++ HAL_Sensores/HAL_BNO055/driver_init.c
@@ -0,0 +1,128 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+
+#include "driver_init.h"
+#include <peripheral_clk_config.h>
+#include <utils.h>
+#include <hal_init.h>
+#include <hpl_gclk_base.h>
+#include <hpl_pm_base.h>
+
+struct usart_sync_descriptor TARGET_IO;
+
+struct i2c_m_sync_desc I2C_BNO055;
+
+void TARGET_IO_PORT_init(void)
+{
+
+	gpio_set_pin_function(PA04, PINMUX_PA04D_SERCOM0_PAD0);
+
+	gpio_set_pin_function(PA05, PINMUX_PA05D_SERCOM0_PAD1);
+}
+
+void TARGET_IO_CLOCK_init(void)
+{
+	_pm_enable_bus_clock(PM_BUS_APBC, SERCOM0);
+	_gclk_enable_channel(SERCOM0_GCLK_ID_CORE, CONF_GCLK_SERCOM0_CORE_SRC);
+}
+
+void TARGET_IO_init(void)
+{
+	TARGET_IO_CLOCK_init();
+	usart_sync_init(&TARGET_IO, SERCOM0, (void *)NULL);
+	TARGET_IO_PORT_init();
+}
+
+void I2C_BNO055_PORT_init(void)
+{
+
+	gpio_set_pin_pull_mode(PA16,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_OFF);
+
+	gpio_set_pin_function(PA16, PINMUX_PA16C_SERCOM1_PAD0);
+
+	gpio_set_pin_pull_mode(PA17,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_OFF);
+
+	gpio_set_pin_function(PA17, PINMUX_PA17C_SERCOM1_PAD1);
+}
+
+void I2C_BNO055_CLOCK_init(void)
+{
+	_pm_enable_bus_clock(PM_BUS_APBC, SERCOM1);
+	_gclk_enable_channel(SERCOM1_GCLK_ID_CORE, CONF_GCLK_SERCOM1_CORE_SRC);
+	_gclk_enable_channel(SERCOM1_GCLK_ID_SLOW, CONF_GCLK_SERCOM1_SLOW_SRC);
+}
+
+void I2C_BNO055_init(void)
+{
+	I2C_BNO055_CLOCK_init();
+	i2c_m_sync_init(&I2C_BNO055, SERCOM1);
+	I2C_BNO055_PORT_init();
+}
+
+void system_init(void)
+{
+	init_mcu();
+
+	// GPIO on PA18
+
+	gpio_set_pin_level(LED_BLUE,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(LED_BLUE, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(LED_BLUE, GPIO_PIN_FUNCTION_OFF);
+
+	// GPIO on PA19
+
+	gpio_set_pin_level(LED_RED,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(LED_RED, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(LED_RED, GPIO_PIN_FUNCTION_OFF);
+
+	// GPIO on PA28
+
+	gpio_set_pin_level(LED_GREEN,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(LED_GREEN, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(LED_GREEN, GPIO_PIN_FUNCTION_OFF);
+
+	TARGET_IO_init();
+
+	I2C_BNO055_init();
+}
